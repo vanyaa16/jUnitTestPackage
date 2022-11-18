85f49f0b3560c9733c3a86b627d899e4b6d428c0
vanyaa16 <96188505+vanyaa16@users.noreply.github.com>
2022-11-18 16:41:54 +0530

    Add files via upload

---
+++ jUnitFunctions.java
@@ -0,0 +1,13 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	
+	public int AddNumbers(int a, int b) {
+		return (a+b);
+	}
+	public String AddStrings(String a, String b) {
+		return (a+b);
+
+	}
+
+}
